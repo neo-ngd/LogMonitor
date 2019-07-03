7a1f518e689cb641e9f9608d07893acf8a355373
zhangtao <ztgoforit@163.com>
2019-07-03 19:10:25 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import api from './api/api';
+
+class Log extends Component {
+  padding(num, length) {
+    for(var len = (num + "").length; len < length; len = num.length) {
+        num = "0" + num;            
+    }
+    return num;
+  }
+  render() {
+    let index = this.padding(this.props.index, 4);
+    return (
+      <div className="log">
+          <p>
+            <span>
+            {`${index}:[${this.props.key}] ${this.props.text}`}
+            </span>
+          </p>
+      </div>
+    );
+  }
+}
+  
+class LogBox extends Component {
+  logHandler = (log, err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    if (this.state.tags.every(ele => ele != log.Name)) {
+      let tags = this.state.tags;
+      tags.push(log.Name);
+      this.setState({tags: tags});
+    }
+    if (this.state.tag != "all" && log.Name != this.state.tag) 
+      return;
+    let index = this.state.data.length + 1;
+    if (10000 < index) {
+        log.Key = 1;
+        this.setState({data: [log]});
+        return;
+    }
+    log.Key = index;
+    let newdata = this.state.data.concat(log);
+    this.setState({data: newdata});
+  }
+  constructor(props) {
+      super(props);
+      this.state = {data: [], tag: "all", tags: ["all"]};
+  }
+  componentWillMount() {
+  }
+  componentDidMount() {
+    api.Regist(this.state.tag, this.logHandler);
+  }
+  render() {
+    return (
+      <div className="logBox">
+        <h1>Log Monitor</h1>
+        <LogList data={this.state.data} />
+      </div>
+    );
+  }
+}
+  
+class LogList extends Component {
+    render = function() {
+      var loglistFunc = this.props.data.map(function(log) {
+        return (
+          <Log text={log.Text} key={log.Key} index={log.Key}/>
+        );
+      });
+      return (
+        <div className="logList">
+          {loglistFunc}
+        </div>
+      );
+    }
+  }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <LogBox url="http://localhost:3001/api/logs" pollInterval={1000} />
     );
   }
 }
---
+++ src/api/api.js
@@ -0,0 +1,41 @@
+var wsServer = 'ws://localhost:8081/ws/';//document.location.href.replace("http", "ws") + "ws/";
+var ws = new WebSocket(wsServer);
+var logger = null
+var message = ""
+ws.onopen = function (e) {
+    console.log("Connected to WebSocket server.",e);
+    if (logger) {
+        ws.send(message);
+    }
+};
+
+ws.onclose = function (e) {
+    console.log("Disconnected",e);
+};
+
+ws.onmessage = function(e) {
+    let logbody = JSON.parse(e.data);
+    if (logger) logger(logbody);
+};
+
+ws.onerror = function (e) {
+    console.log('Error occured: ' + e.data,e);
+};
+
+exports.Regist = function(name, cb) {
+    console.log("regist");
+    logger = cb;
+    console.log(ws.OPEN, ws.CONNECTING, ws.CLOSED, ws.CLOSING);
+    console.log(ws.readyState);
+    if (ws.readyState === ws.OPEN) {
+        console.log("send");
+        ws.send(name);
+        return;
+    }
+    console.log("store");
+    message = name;
+};
+
+exports.Close = function() {
+    ws.close();
+};
